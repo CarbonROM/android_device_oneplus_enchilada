7220e0ce5b253b0e9305d6fe5e8ab2d225704552
Christian Oder <myself5@carbonrom.org>
2023-05-25 01:08:47 +0200

    enchilada: Add CarbonROM configs

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_enchilada.mk
+    $(LOCAL_DIR)/carbon_enchilada.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_enchilada-user \
-    lineage_enchilada-userdebug \
-    lineage_enchilada-eng
+    carbon_enchilada-user \
+    carbon_enchilada-userdebug \
+    carbon_enchilada-eng
